314312063aebe5e37073390c5a69ad48ecb714ca
reviakin@voider <reviakin@yamaldorstroy.ru>
2019-11-13 10:07:48 +0500

    РґРѕР±Р°РІР»РµРЅР° РІРѕР·РјРѕР¶РЅРѕ РїРµСЂРµРґР°С‚СЊ РЅРѕРІС‹Р№ РїР°СЂРѕР»СЊ

--- usr_revive.cmd
+++ usr_revive.cmd
@@ -1,1 +1,1 @@
-powershell.exe -noprofile -executionpolicy bypass -file \\chl-proxy\IT\usermanagement\usr_revive.ps1 %1
+powershell.exe -noprofile -executionpolicy bypass -file \\chl-proxy\IT\usermanagement\usr_revive.ps1 %1 %2
